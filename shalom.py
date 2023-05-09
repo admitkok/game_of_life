a4ba3cf0440a7580589fa9a494cba76cc082f312
aleksandrkokov <kokov.alexander@gmail.com>
2023-05-09 12:25:08 +0200

    wk console game of life

--- shalom.py
+++ shalom.py
@@ -10,58 +10,89 @@ class Board:
 
     def __repr__(self):
         for i in range(m):
-            return (self.cells[i])
+            return self.cells[i]
 
     def next_step(self):
         self.cells1 = deepcopy(self.cells)
         for i in range(self.hight):
             for j in range(self.width):
-                if (i - 1 < 0):
-                    if (j - 1 < 0):
-                        if (self.cells[i + 1][j].state + self.cells[i][j + 1].state == 2):
+                if i - 1 < 0:
+                    if j - 1 < 0:
+                        if self.cells[i + 1][j].state + self.cells[i][j + 1].state == 2:
                             self.cells1[i][j].state = 1
                         else:
                             self.cells1[i][j].state = 0
-                    elif (j + 1 >= m):
-                        if (self.cells[i + 1][j].state + self.cells[i][j- 1].state == 2):
+                    elif j + 1 >= m:
+                        if self.cells[i + 1][j].state + self.cells[i][j - 1].state == 2:
                             self.cells1[i][j].state = 1
                         else:
                             self.cells1[i][j].state = 0
                     else:
-                        if 2 <= self.cells[i+ 1][j].state + self.cells[i][j + 1].state + self.cells[i][j - 1].state <= 3:
+                        if (
+                            2
+                            <= self.cells[i + 1][j].state
+                            + self.cells[i][j + 1].state
+                            + self.cells[i][j - 1].state
+                            <= 3
+                        ):
                             self.cells1[i][j].state = 1
                         else:
                             self.cells1[i][j].state = 0
 
-                elif (i + 1 >= n):
-                    if (j - 1 < 0):
-                        if (self.cells[i - 1][j].state + self.cells[i][j + 1].state == 2):
+                elif i + 1 >= n:
+                    if j - 1 < 0:
+                        if self.cells[i - 1][j].state + self.cells[i][j + 1].state == 2:
                             self.cells1[i][j].state = 1
                         else:
                             self.cells1[i][j].state = 0
-                    elif (j + 1 >= m):
-                        if (self.cells[i - 1][j].state + self.cells[i][j - 1].state == 2):
+                    elif j + 1 >= m:
+                        if self.cells[i - 1][j].state + self.cells[i][j - 1].state == 2:
                             self.cells1[i][j].state = 1
                         else:
                             self.cells1[i][j].state = 0
                     else:
-                        if 2 <= self.cells[i - 1][j].state + self.cells[i][j + 1].state + self.cells[i][j- 1].state <= 3:
+                        if (
+                            2
+                            <= self.cells[i - 1][j].state
+                            + self.cells[i][j + 1].state
+                            + self.cells[i][j - 1].state
+                            <= 3
+                        ):
                             self.cells1[i][j].state = 1
                         else:
                             self.cells1[i][j].state = 0
 
-                elif (j - 1 < 0 and 1 <= i < n - 1):
-                    if 2 <= self.cells[i - 1][j].state + self.cells[i][j + 1].state + self.cells[i + 1][j].state <= 3:
+                elif j - 1 < 0 and 1 <= i < n - 1:
+                    if (
+                        2
+                        <= self.cells[i - 1][j].state
+                        + self.cells[i][j + 1].state
+                        + self.cells[i + 1][j].state
+                        <= 3
+                    ):
                         self.cells1[i][j].state = 1
                     else:
                         self.cells1[i][j].state = 0
-                elif (j + 1 >= m and 1 <= i < n - 1):
-                    if 2 <= self.cells[i - 1][j].state + self.cells[i][j - 1].state + self.cells[i + 1][j].state <= 3:
+                elif j + 1 >= m and 1 <= i < n - 1:
+                    if (
+                        2
+                        <= self.cells[i - 1][j].state
+                        + self.cells[i][j - 1].state
+                        + self.cells[i + 1][j].state
+                        <= 3
+                    ):
                         self.cells1[i][j].state = 1
                     else:
                         self.cells1[i][j].state = 0
-                elif (1 <= i < n - 1 and 1 <= j < m - 1):
-                    if 2 <= self.cells[i - 1][j].state + self.cells[i][j- 1].state + self.cells[i+ 1][j].state + self.cells[i][j + 1].state <= 3:
+                elif 1 <= i < n - 1 and 1 <= j < m - 1:
+                    if (
+                        2
+                        <= self.cells[i - 1][j].state
+                        + self.cells[i][j - 1].state
+                        + self.cells[i + 1][j].state
+                        + self.cells[i][j + 1].state
+                        <= 3
+                    ):
                         self.cells1[i][j].state = 1
                     else:
                         self.cells1[i][j].state = 0
@@ -75,13 +106,13 @@ class Cell(Board):
         self.col = col
 
     def __repr__(self):
-        return f'{self.state}'
+        return f"{self.state}"
 
     def is_alive(self, brd):
         if brd[self.row][self.col].state == 1:
-            return 'Alive'
+            return "Alive"
         else:
-            return 'Dead'
+            return "Dead"
 
 
 n = random.randint(3, 10)
@@ -99,7 +130,7 @@ for i in range(n):
 for i in range(n):
     print(*bd[i])
 
-print(bd[random.randint(0, n-1)][random.randint(0, m-1)].is_alive(b1.cells))
+print(bd[random.randint(0, n - 1)][random.randint(0, m - 1)].is_alive(b1.cells))
 
 b1.next_step()
 
